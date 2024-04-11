f450787fd1fb020df25842b41c8da24c0a1ebe79
Raushan <raushankumar606@gmail.com>
2024-04-11 14:42:01 +0530

    first commit

---
+++ src/main/java/fish/payara/cloud/ci/demo/CidemoRestApplication.java
@@ -0,0 +1,13 @@
+package fish.payara.cloud.ci.demo;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+@ApplicationScoped
+public class CidemoRestApplication extends Application {
+}
